f14010e840bc542fc6553a73c5da4c37bbf6ebe2
Dhruv1238 <dhruv4075@gmail.com>
2024-02-16 20:33:11 +0530

    feat: API Scraper | | Data appended to excel

--- api_scrapper/api_scraping_script.py
+++ api_scrapper/api_scraping_script.py
@@ -8,5 +8,33 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
+BASE_URL = "https://remoteok.com/api"
+USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
+REQUEST_HEADERS = {
+    "User-Agent": USER_AGENT,
+    "Accept-Language": "en-US,en;q=0.5"
+}
+
+def get_job_postings():
+    res=requests.get(BASE_URL, headers=REQUEST_HEADERS)
+    return res.json()
+
+def output_to_xlsx(data):
+    wb = Workbook()
+    job_sheet = wb.add_sheet('Jobs_Sheet')
+    header = list(data[0].keys())
+    for i in range(len(header)):
+        job_sheet.write(0, i, header[i])
+
+    for i in range(len(data)):
+        job = data[i]
+        values = list(job.values())
+        for x in range(len(values)):
+            job_sheet.write(i+1, x, values[x])
+    wb.save('job_postings.xls')
+
+
 if __name__=='__main__':
-    print("Hello!")
+    json= get_job_postings()
+    output_to_xlsx(json[1:])
+    # print(json[1])
